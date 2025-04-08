24145ac048e4b01c5f77a566c48b7c0ad030fcb0
Mona Lisa <hiroakiyui@gmail.com>
2025-04-09 08:03:11 +0900

    fix e5

--- dl/download_e5.py
+++ dl/download_e5.py
@@ -1,7 +1,7 @@
 from sentence_transformers import SentenceTransformer
 
 # 保存先ディレクトリ（絶対パス）
-model_path = "/Users/hiroakiyui/Documents/dev/llm-env"
+model_path = "/Users/xxxxxxx/Documents/dev/llm-env"
 
 # e5-small のダウンロード＆保存
 model = SentenceTransformer("intfloat/e5-small", cache_folder=model_path)
