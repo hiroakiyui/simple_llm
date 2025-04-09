aa118a1e0de773e43c6876fe0125500e22257817
Mona Lisa <hiroakiyui@gmail.com>
2025-04-09 12:53:08 +0900

    fix e5

--- dl/download_e5.py
+++ dl/download_e5.py
@@ -6,5 +6,5 @@ model_path = "/Users/<ユーザー名>/Documents/dev/llm-env"
 # e5-small のダウンロード＆保存
 model = SentenceTransformer("intfloat/e5-small", cache_folder=model_path)
 
-print("✅ e5-small モデルをダウンロードしました")
+print("✅ e5-small モデルをダウンロードしました。")
 
